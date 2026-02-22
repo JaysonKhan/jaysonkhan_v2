74785358abaa28e1bf7e5b15e82c71caed0dbcde
Jahongir KHAN <bettaxacker@gmail.com>
2026-02-22 22:43:49 +0500

    fix

--- backend/apps/interactions/admin.py
+++ backend/apps/interactions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import TelegramProfile, Comment, Like
+from .models import TelegramProfile, Comment, Like, CommentReaction
 
 
 @admin.register(TelegramProfile)
