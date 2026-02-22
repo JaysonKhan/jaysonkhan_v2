715e95fa764d31d14602ee129a7f484fe9d18685
Jahongir KHAN <bettaxacker@gmail.com>
2026-02-22 20:27:26 +0500

    update

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 # wsgi.py defaults to prod. To use these settings locally, run:
 #   DJANGO_SETTINGS_MODULE=config.settings.dev python manage.py runserver
 # Or set it in your shell: export DJANGO_SETTINGS_MODULE=config.settings.dev
-DEBUG = True
+DEBUG = False
 
 # Allow all hosts in development
 ALLOWED_HOSTS = ['*']
