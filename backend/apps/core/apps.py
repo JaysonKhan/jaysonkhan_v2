c63f17cdc81c4e6e325d4ac6f228bb8046da5c61
Jahongir KHAN <bettaxacker@gmail.com>
2026-04-26 10:33:04 +0500

    debug: log at request time which dict is seen

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -38,3 +38,15 @@ class CoreConfig(AppConfig):
             new = _LangInfoDict(_locale.LANG_INFO)
             _locale.LANG_INFO = new
             _trans_real.LANG_INFO = new
+            import sys as __sys
+            __sys.stderr.write(f"[ready] After replace: locale.LANG_INFO id={id(_locale.LANG_INFO)} type={type(_locale.LANG_INFO).__name__}\n")
+
+        # Wrap get_language_info to log at request time which dict is seen.
+        from django.utils import translation as _t
+        _orig_gli = _t.get_language_info
+        def _logging_gli(lang_code):
+            import sys as ___s
+            import django.conf.locale as ___L
+            ___s.stderr.write(f"[gli-call] {lang_code!r} locale.LANG_INFO id={id(___L.LANG_INFO)} type={type(___L.LANG_INFO).__name__} has_xo={('xo' in ___L.LANG_INFO)}\n")
+            return _orig_gli(lang_code)
+        _t.get_language_info = _logging_gli
